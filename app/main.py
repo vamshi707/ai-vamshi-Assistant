4365072df17be264421855d21d5b01b80dbdf39d
vamshi707 <vamshivmc143@gmail.com>
2026-07-18 21:56:02 +0530

    Fix CORS for Netlify

--- app/main.py
+++ app/main.py
@@ -12,6 +12,7 @@ app.add_middleware(
         "http://localhost:5174",
         "http://127.0.0.1:5173",
         "http://127.0.0.1:5174",
+        "https://radiant-alfajores-e3d31a.netlify.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
