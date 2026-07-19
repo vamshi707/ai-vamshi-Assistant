45ee662a48c50c34799aad34e2dd624144ec3e25
vamshi707 <vamshivmc143@gmail.com>
2026-07-20 00:10:07 +0530

    we changed like maily.py file for the android backend purpose

--- app/main.py
+++ app/main.py
@@ -8,6 +8,10 @@ app = FastAPI(title="AI Assistant")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
+
+        "http://localhost",
+        "capacitor://localhost",
+        
         "http://localhost:5173",
         "http://localhost:5174",
         "http://127.0.0.1:5173",
